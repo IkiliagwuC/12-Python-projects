bf8faf00bd92c687faafa7410fce969803431e7d
Ikiliagwu Chukwudi <57571996+IkiliagwuC@users.noreply.github.com>
2022-09-19 03:33:28 +0100

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+#madlibs game using fstrings.
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("verb: ")
+famous_person = input("enter a famous person: ")
+
+madlibs = f"Computer programming is so {adj} it makes me so excited\
+all the time because I love to {verb1}. Stay hydrated and \
+{verb2} like you are a {famous_person}"
+
+print(madlibs)
